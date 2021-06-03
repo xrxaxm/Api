02a0e2fb367515cc2c469539fabf2246933a559e
aripjuniarto123 <46416641+aripjuniarto123@users.noreply.github.com>
2021-06-03 14:25:05 +0000

    Create api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "@only_fxc7"
+var creator = "@ArifGans"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["Fxc7", "manogay"];
+const listkey = ["ArifGans"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
